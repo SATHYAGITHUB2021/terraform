6bdacf2a1eb5a10df04d1234172f190eaff4ccff
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-19 11:28:51 +0530

    created and added content in sample.tf

--- 01-output/sample.tf
+++ 01-output/sample.tf
@@ -18,3 +18,35 @@ variable "sample" {
 // var.sample can be used without quotes
 // ${var.sample} is used within quotes
 // Usually, ${} uses when we combine with some other data
+// Terraform does not support single quote at any level.Only supports double quotes.
+
+# Variable Data types
+# String data type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean data type
+variable "sample3" {
+  default = yes
+}
+
+# Variable type list variable
+
+variable "sample4" {
+  default = [
+    "Hello",
+    1000,
+    yes,
+    "World"
+  ]
+}
+
+output "sample4" {
+  value = var.sample4[0]
+}

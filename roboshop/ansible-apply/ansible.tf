698f6ee3bc1e5faa0c7070901b0534bc631741c3
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-20 19:50:06 +0530

    added content in roboshop/terraform

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -1,6 +1,10 @@
 resource "null_resource" "ansible-apply" {
+  triggers = {
+    abc = timestamp()
+  }
   count            = length(var.COMPONENTS)
   provisioner "remote-exec" {
+
     connection {
       host         = "${element(var.COMPONENTS, count.index)}.roboshop.internal"
       user         = "centos"

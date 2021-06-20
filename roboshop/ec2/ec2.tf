681fff563ba92800a4b5675c004709da49f447a5
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-20 15:48:37 +0530

    added content in roboshop/state.tf

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "sample" {
+resource "aws_spot_instance_request" "frontend" {
   ami                    = "ami-059e6ca6474628ef0"
   spot_price             = "0.0031"
   instance_type          = "t3.micro"
+
   tags = {
     Name = "frontend"
   }
 }
-
 provider "aws" {
   region = "us-east-1"
 }

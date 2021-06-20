cc734261952a20ca1cd5ef50334c0ae3f223a27c
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-20 17:18:37 +0530

    added content in roboshop/state.tf

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -14,7 +14,14 @@ provider "aws" {
 
 variable "COMPONENTS" {}
 
-resource "time_sleep" "wait_30_seconds" {
+resource "time_sleep" "wait" {
   depends_on            = [aws_spot_instance_request.cheap_worker]
   create_duration       = "120s"
+}
+
+resource "aws_ec2_tag" "example" {
+  count                 = length(var.COMPONENTS)
+  resource_id           = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key                   = "Name"
+  value                 = element(var.COMPONENTS, count.index)
 }

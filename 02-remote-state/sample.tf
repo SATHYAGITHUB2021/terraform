9155019dd928738d8780b5d633bac82fbad685a6
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-20 11:06:31 +0530

    added content in remote-state/sample.tf

--- 02-remote-state/sample.tf
+++ 02-remote-state/sample.tf
@@ -2,3 +2,15 @@ resource "aws_instance" "sample" {
   ami                    = "ami-059e6ca6474628ef0"
   instance_type          = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket   = "sathya-aws-bucket"
+    key      = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}

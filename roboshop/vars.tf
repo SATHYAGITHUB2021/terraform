95a816488836ecc2253061ebb8a174bbf04df293
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-07-03 17:25:08 +0530

    added components in vars.tf

--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -1,3 +1,7 @@
+//variable "COMPONENTS" {
+//  default = ["frontend", "mongodb", "catalogue", "redis", "cart", "user", "mysql", "shipping", "rabbitmq", "payment"]
+//}
+
 variable "COMPONENTS" {
-  default = ["frontend", "mongodb", "catalogue", "redis", "cart", "user", "mysql", "shipping", "rabbitmq", "payment"]
+  default = ["cart"]
 }
